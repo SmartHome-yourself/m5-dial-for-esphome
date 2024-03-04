f73e57e7502eac92c1fc8ff43604e9d2205716f5
ArduFox <47301786+ArduFox@users.noreply.github.com>
2024-03-04 18:12:01 +0100

    Update m5dial_display.h

--- components/shys_m5_dial/m5dial_display.h
+++ components/shys_m5_dial/m5dial_display.h
@@ -100,7 +100,7 @@ namespace esphome
 
                     if(currentDevice.isDimmEnabled()){
                         gfx->fillRect(0, 0, width, height-(height*currentValue/100), 0x8080B0); // RED);
-                        gfx->fillRect(0, height-(height*currentValue/100), width, height, , 0xFFE080); // YELLOW);
+                        gfx->fillRect(0, height-(height*currentValue/100), width, height, 0xFFE080); // YELLOW);
 
                         gfx->setTextSize(3);
                         gfx->drawString(String(currentValue),
